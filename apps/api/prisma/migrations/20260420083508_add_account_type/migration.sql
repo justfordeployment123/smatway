ac5c0b5f55a3aac45f17f63f61c5b531457e3b09
Muhammad Hasaam <m.hasaam.official@gmail.com>
2026-04-21 22:30:34 +0500

    remove extra .sql files

--- apps/api/prisma/migrations/20260420083508_add_account_type/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "AccountType" AS ENUM ('TRAVELER', 'TRANSPORTER');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "accountType" "AccountType";
--- apps/api/prisma/migrations/20260421000000_add_user_profile_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "User"
-ADD COLUMN     "phoneNumber" TEXT,
-ADD COLUMN     "country" TEXT;
